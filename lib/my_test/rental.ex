a8f2e583af1002406b7e06f5b76b0292e7730646
Josh Howard <jashghoward@icloud.com>
2023-08-30 22:01:44 +0200

    rental.ex update

--- lib/my_test/rental.ex
+++ lib/my_test/rental.ex
@@ -76,19 +76,19 @@ defmodule MyTest.Rental do
     |> Repo.all()
   end
 
-  def rent_movie() do
-    Repo.transaction(fn ->
-      rental_count_query = from(r in Rental, where: r.customer_id == ^customer_id, select: count(r.id))
+  # def rent_movie() do
+  #   Repo.transaction(fn ->
+  #     rental_count_query = from(r in Rental, where: r.customer_id == ^customer_id, select: count(r.id))
 
-      case Repo.aggregate(rental_count_query, :count) do
-        count when count >= 2 ->
-          IO.puts("Customer has reached the maximum rental limit")
-        _ ->
-          rental_attrs = %{customer_id: customer_id, movie_id: movie_id, date_rented: current_datetime_utc}
-          Repo.insert!(Rental.changeset(%Rental{}, rental_attrs))
-          IO.puts("Movie rented successfully")
-      end
-    end)
+  #     case Repo.aggregate(rental_count_query, :count) do
+  #       count when count >= 2 ->
+  #         IO.puts("Customer has reached the maximum rental limit")
+  #       _ ->
+  #         rental_attrs = %{customer_id: customer_id, movie_id: movie_id, date_rented: current_datetime_utc}
+  #         Repo.insert!(Rental.changeset(%Rental{}, rental_attrs))
+  #         IO.puts("Movie rented successfully")
+  #     end
+  #   end)
   end
 end
 
