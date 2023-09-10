4130defb9c7bb1e8e854e4fddea20340e2a2ed79
Josh Howard <jashghoward@icloud.com>
2023-09-10 21:32:22 +0200

    customer.ex update

--- lib/my_test/movie.ex
+++ lib/my_test/movie.ex
@@ -23,6 +23,8 @@ defmodule MyTest.Movie do
     query = from(m in Movie, join: s in assoc(m, :shop), select: {m.title, s.name})
     movies_with_shop_names = Repo.all(query)
 
+
+
   # Print the list of movies and their associated shop names
     IO.puts("List of movies with their associated shop names:")
     Enum.each(movies_with_shop_names, fn {movie_title, shop_name} ->
