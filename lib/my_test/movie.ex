3eb4f8b7f52cc87bfe24b1f1881d7b130268f3eb
Josh Howard <jashghoward@icloud.com>
2023-10-04 20:19:28 +0200

    Movie.ex update

--- lib/my_test/movie.ex
+++ lib/my_test/movie.ex
@@ -23,8 +23,6 @@ defmodule MyTest.Movie do
 
   def list_movie do
     query = from(m in Movie, join: s in assoc(m, :shop), select: {m.title, s.name})
-
-
     movies_with_shop_names = Repo.all(query)
   # Print the list of movies and their associated shop names
     IO.puts("List of movies with their associated shop names:")
