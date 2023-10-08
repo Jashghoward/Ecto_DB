e0025b5dc2df554f5a51730f4eadf8de53c2c448
Josh Howard <jashghoward@icloud.com>
2023-10-08 23:35:11 +0200

    Customer.ex update

--- lib/my_test/customer.ex
+++ lib/my_test/customer.ex
@@ -18,6 +18,8 @@ defmodule MyTest.Customer do
   end
 
   def create_customer do
+
+
       new_customer = %Customer{firstname: "Brad", lastname: "Howard"} # Replace 1 with the actual shop ID
       Repo.insert!(new_customer)
       IO.puts("New movie added successfully.")
